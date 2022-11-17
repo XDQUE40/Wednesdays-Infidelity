c0209dce714451ae30ef0fca465197e1a9dc0f5c
XDQUE40 <116643129+XDQUE40@users.noreply.github.com>
2022-11-17 15:03:33 -0300

    Update CppAPI.hx

--- source/data/CppAPI.hx
+++ source/data/CppAPI.hx
@@ -10,22 +10,30 @@ class CppAPI
 
 	public static function darkMode()
 	{
+	    #if desktop 
 		WindowsData.setWindowColorMode(DARK);
+		#end
 	}
 
 	public static function lightMode()
 	{
+	    #if desktop 
 		WindowsData.setWindowColorMode(LIGHT);
+		#end 
 	}
 
 	public static function setWindowOppacity(a:Float)
 	{
+	    #if desktop 
 		WindowsData.setWindowAlpha(a);
+		#end 
 	}
 
 	public static function _setWindowLayered()
 	{
+	    #if desktop 
 		WindowsData._setWindowLayered();
+		#end
 	}
 	#end
 }
