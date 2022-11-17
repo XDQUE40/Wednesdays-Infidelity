1138073ae3b3e357de2e02ac6de7bda7427f152c
XDQUE40 <116643129+XDQUE40@users.noreply.github.com>
2022-11-17 14:01:07 -0300

    Update FreeplayState.hx

--- source/states/menus/FreeplayState.hx
+++ source/states/menus/FreeplayState.hx
@@ -256,6 +256,10 @@ class FreeplayState extends MusicBeatState
 		add(daStatic);
 		daStatic.animation.play('static');
 
+		#if android
+		addVirtualPad(LEFT_FULL, A_B);
+		#end
+		
 		super.create();
 	}
 
