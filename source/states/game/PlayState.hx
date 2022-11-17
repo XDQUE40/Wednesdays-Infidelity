6c3465b62312c35939fc02cfc25406530984f443
XDQUE40 <116643129+XDQUE40@users.noreply.github.com>
2022-11-17 16:20:44 -0300

    Shit 🐒

--- source/states/game/PlayState.hx
+++ source/states/game/PlayState.hx
@@ -1226,27 +1226,14 @@ class PlayState extends MusicBeatState
 		botplayTxt.borderSize = 1.25;
 		botplayTxt.visible = cpuControlled;
 		add(botplayTxt);
-		
-	var creditTxt = new FlxText(876, 648, 348);
-	creditTxt.text = "PORTED BY\nDanizin";
-	 creditTxt.setFormat(Paths.font("vcr.ttf"), 30, FlxColor.WHITE, RIGHT, FlxTextBorderStyle.OUTLINE,FlxColor.BLACK);
-	creditTxt.scrollFactor.set();
-	add(creditTxt);
-	
 	if (ClientPrefs.downScroll)
 		     botplayTxt.y = timeBarBG.y - 78;
 	}		
 		
-             if(ClientPrefs.downScroll) {
-		                   creditTxt.y = 148;
-             }	                  
-
-
 		strumLineNotes.cameras = [camHUD];
 		grpNoteSplashes.cameras = [camHUD];
 		notes.cameras = [camHUD];
 		healthBar.cameras = [camHUD];
-		creditTxt.cameras = [camHUD];
 	  healthBarBG.cameras = [camHUD];
 		iconP1.cameras = [camHUD];
 		iconP2.cameras = [camHUD];
