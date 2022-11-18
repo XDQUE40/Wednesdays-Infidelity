cf265c66f7a13b1c691f60a7c6f4907669a400e1
XDQUE40 <116643129+XDQUE40@users.noreply.github.com>
2022-11-17 23:43:16 -0300

    Update FreeplayState.hx

--- source/states/menus/FreeplayState.hx
+++ source/states/menus/FreeplayState.hx
@@ -378,7 +378,7 @@ class FreeplayState extends MusicBeatState
 			var downP = controls.UI_DOWN_P;
 			var accepted = controls.ACCEPT;
 			var space = FlxG.keys.justPressed.SPACE
-			var ctrl = FlxG.keys.justPressed.CONTROL;
+			var ctrl = FlxG.keys.justPressed.CONTROL
 
 			var shiftMult:Int = 1;
 			if (FlxG.keys.pressed.SHIFT || controls.RESET)
