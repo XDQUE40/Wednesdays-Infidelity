8728e3ec3ad7cc9f0160055b0bd2832880e08fda
XDQUE40 <116643129+XDQUE40@users.noreply.github.com>
2022-11-17 23:37:32 -0300

    Update FreeplaySelectorState.hx

--- source/states/menus/FreeplaySelectorState.hx
+++ source/states/menus/FreeplaySelectorState.hx
@@ -190,24 +190,24 @@ class FreeplaySelectorState extends MusicBeatState
 			}*/
 			
 			for (touch in FlxG.touches.list) {
-				if (controls.UI_UP_P || (touch.overlaps(_virtualpad.buttonUp) && touch.justPressed))
+				if (controls.UI_UP_P || (touch.overlaps && touch.justPressed))
 				{
 					FlxG.sound.play(Paths.sound('scrollMenu'));
 					changeItem(-1);
 				}
-				if (controls.BACK || (touch.overlaps(_virtualpad.buttonB) && touch.justPressed))
+				if (controls.BACK || (touch.overlaps && touch.justPressed))
 				{
 					FlxG.sound.play(Paths.sound('cancelMenu'));
 					MusicBeatState.switchState(new MainMenuState());
 				}
 
-				if (controls.UI_DOWN_P || (touch.overlaps(_virtualpad.buttonDown) && touch.justPressed))
+				if (controls.UI_DOWN_P || (touch.overlaps && touch.justPressed))
 				{
 					FlxG.sound.play(Paths.sound('scrollMenu'));
 					changeItem(1);
 				}
 
-				if (controls.ACCEPT || (touch.overlaps(_virtualpad.buttonA) && touch.justPressed))
+				if (controls.ACCEPT || (touch.overlaps && touch.justPressed))
 				{
 					selectedSomethin = true;
 					FlxG.sound.play(Paths.sound('confirmMenu'));
